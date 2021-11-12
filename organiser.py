d71b55c03248b8eae85dac85f606b5c13265454e
cronos-hash <apollo@dnmx.org>
2021-11-12 16:55:44 +0000

    sort, search add (no functions) , fix index errors

---
+++ organiser.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3 
+import os
+import sys
+a = open('default_list')
+current_list = a.readlines()
+print("CURRENT LIST SELECTED : " + str(current_list))
+a.close()
+try:
+    if sys.argv[1] == '-h':
+        print("search commands:")
+        print("ex:  command.py -S basic <query>")
+        print("run searched command")
+        print("ex:  command.py -S run <query>")
+        print("add command + note")
+        print("ex:  command.py -a")
+        print("change list")
+        print("ex: command.py -c")
+        print("print current list")
+        print("ex: command.py -cC")
+except IndexError:
+    print(" ")
+if sys.argv[1] == '-c':
+    IndexError = False
+    os.system("rm -r default_list")
+    commandlist = input("commandlist?: ")
+    os.system("touch " + commandlist)
+    if commandlist == 'default':
+        commandlist == 'commands_list_commands'
+        os.system("echo 'commands_list_commands' > default_list")
+    elif commandlist != 'default':
+        commandlist == commandlist
+        os.system("echo '" + commandlist + "' >> default_list")
+if sys.argv[1] == '-cC':
+    a = open('default_list')
+    current_list = a.readlines()
+    print(current_list)
+    a.close()
+if sys.argv[1] == '-S':
+    if sys.argv[2] == "basic":
+        a = open('default_list')
+        commandlistlines = a.readlines()
+        selected_list = commandlistlines[0]
+        a.close()
+        with open(selected_list, "r") as filehandle:
+            result = [line.strip() for line in filehandle if sys.argv[3] in line]
+            for x in result:
+                print(x)
+    if sys.argv[2] == "run":
+        if commandlistlines[0] == 'commands_list_commands':
+            a_file = open("commands_list_commands","r")
+            for number, line in enumerate(a_file):
+                phrase = sys.argv[3]
+                if phrase in line:
+                    line_number = number
+                    break
+        a_file.close()
+        f = open('commands_list_main')
+        lines=f.readlines()
+        print("attempting to run command:")
+        print(lines[line_number])
+        os.system(lines[line_number])
+        f.close()
+        os.system("")
+        if commandlistlines[0] != 'commands_list_commands':
+            a_file = open(commandlistlines[0], "r")
+            for number, line in enumerate(a_file):
+                phrase = sys.argv[3]
+                if phrase in line:
+                    line_number = number
+                    break
+        a_file.close()
+        f = open(commandlistlines[0])
+        lines = f.readlines()
+        print("attempting to run command:")
+        print(lines[line_number])
+        f.close()
+        os.system("")
+if sys.argv[1] == '-a':
+    new_command = input("add command: ")
+    comment = input("add comment: ")
+    a = open('default_list')
+    commandlistlines = a.readlines()
+    list = commandlistlines[0]
+    if list == 'commands_list_commands':
+        os.system("echo '" + new_command + "' >> commands_list_main")
+        os.system("echo '" + new_command + "     :" + comment +"' >> commands_list_commands")
+    else:
+        os.system("echo '" + new_command + "' >> " + list + "")
+else:
+    print("try organiser.py -h")
